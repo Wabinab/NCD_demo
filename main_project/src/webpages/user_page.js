49824c798394ef4c18ff283218eb79ccad028b08
Ubuntu <azureuser@dappletTemp.xhtobzropgwu3b4uquo01n14gb.cx.internal.cloudapp.net>
2022-03-08 14:25:22 +0000

    changes to header

--- main_project/src/webpages/user_page.js
+++ main_project/src/webpages/user_page.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "../global.css"
 import "./local.css"
+import NearLogo from "./assets/near-black.svg";
 
 const user_page_render = (login) => {
   return (
     <main>
-        <div class="topnav">
-          <div class="topnav-right">
-            <button onClick={login}>Sign in</button>
-          </div>
-          <img src={require('/assets/near.png')} width="2%" height="2%"/>
-          <h1>Some Title</h1>
+      <div class="topnav">
+        <div class="logo">
+          <img src={NearLogo} alt="NEAR Logo" height="30%"/>
+          <h1> NEAR Blog</h1>
         </div>
-
-      
+        <ul class="middle">
+          <li><h2>User Profile</h2></li>
+        </ul>
+        <ul class="no-bullets">
+          <li><button onClick={login}>Sign in</button></li> 
+        </ul>
+      </div>  
 
       <div>
         <p>
